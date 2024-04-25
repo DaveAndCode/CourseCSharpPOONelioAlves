145e7efc08766918b11da77184316cb23fe902a5
DaveAndCode <DaveAndCode@gmail.com>
2024-04-25 10:41:25 -0300

    Primeiro programa C#

---
+++ PrimeiroPrograma/Program.cs
@@ -0,0 +1,16 @@
+﻿// Essa é a estrutura básica de um programa em C# (Legado) - Fica mais visível a estrutura da linguagem
+using System;  // Essa lib já é importada automaticamente
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo!");
+        }
+    }
+}
+
+// Depois do .NET 6.0, é possível usar um programa semelhante ao Python (script)
+// Console.WriteLine("Olá, Mundo!");  - Para isso, a estrutura inicial não deve existir
