040a304ae0ec3480925de91ad8f88c1042ce17a0
DaveAndCode <DaveAndCode@gmail.com>
2024-04-28 20:09:05 -0300

    Entrada de dados em C# - PARTE 1

---
+++ EntradaDeDados/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? frase = Console.ReadLine();  // o interrogação ? que dizer que possivelmente é um nullable type
+            string? x = Console.ReadLine();
+            string? y = Console.ReadLine();
+            string? z = Console.ReadLine();
+            string[] vet = Console.ReadLine().Split(' ');  // Aqui foi declarado um vetor de strings (lista) que será separado os index por espaços
+            string? p1 = vet[0];
+            string? p2 = vet[1];
+            string? p3 = vet[2];
+
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(p1 + " " + p2 + " " + p3);
+        }
+    }
+}
