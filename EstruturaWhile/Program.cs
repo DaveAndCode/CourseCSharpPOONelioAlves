38b7f3413641b98db3b93cee73fe187184fca470
DaveAndCode <DaveAndCode@gmail.com>
2024-05-01 16:11:37 -0300

    Estrutura repetitiva enquanto (while)

---
+++ EstruturaWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+namespace EstruturaWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            double numero = double.Parse(Console.ReadLine()!);
+
+            while (numero >= 0)
+            {
+                double raizQ = Math.Sqrt(numero);
+                Console.WriteLine(raizQ.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite outro número: ");
+                numero = double.Parse(Console.ReadLine()!);
+            }
+
+            Console.WriteLine("Número negativo!");
+            
+        }
+    }
+}
