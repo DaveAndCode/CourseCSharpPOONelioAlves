b08242f1099531b37cc668eeb58a5d894d393fe3
DaveAndCode <DaveAndCode@gmail.com>
2024-05-02 13:42:25 -0300

    Estrutura repetitiva para (for)

---
+++ EstruturaFor/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EstruturaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            for (início ; condição ; incremento)
+            {
+                comando 1
+                comando 2
+            }
+            */
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int N = int.Parse(Console.ReadLine()!);
+            int soma = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"Valor: #{i}");
+                int valor = int.Parse(Console.ReadLine()!);
+                soma += valor;
+            }
+
+            System.Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
