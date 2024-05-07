037da7dd5d542056b982a42b6042797adc544f9b
DaveAndCode <DaveAndCode@gmail.com>
2024-05-07 10:54:01 -0300

    Sobrecarga

---
+++ OrientacaoObjetos/Sobrecarga/Produto.cs
@@ -0,0 +1,52 @@
+using System.Globalization;
+namespace Sobrecarga
+{
+    class Produto
+    {
+      public string? Nome;
+        public double Preco;
+        public int Quantidade;
+
+        // Método construtor
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        // Isso é uma sobrecarga de construtor, há duas formas de se instanciar o objeto, passando parâmetros diferentes.
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 0;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ OrientacaoObjetos/Sobrecarga/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+namespace Sobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sobrecarga
+            /*
+            - É um recurso que uma classe possui de oferecer mais de uma operação com o mesm onome, porém com diferentes listas de parâmetros.
+            */
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+            Produto p = new(nome, preco);  // Agora na instanciação deverá ser passado os argumentos dos parâmetros declarados no construtor
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine()!);
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine()!);
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
