76c270a3f741e3b48cb93717801d8dc2fe8ff27a
DaveAndCode <DaveAndCode@gmail.com>
2024-05-09 18:24:05 -0300

    Correção do exercício Ex04Fixacao - PARTE 1

--- Exercicios/OrientacaoObjetos/Ex04Fixacao.cs
+++ Exercicios/OrientacaoObjetos/Ex04Fixacao.cs
@@ -1,57 +1,72 @@
+using System.Globalization;
+
 namespace OrientacaoObjetos
 {
     class Ex04Fixacao
     {
         public static void Solucao()
         {
-            Conta conta01 = new Conta();
+            ContaBancaria conta;
 
             Console.Write("Entre o número da conta: ");
-            conta01.ContaId = int.Parse(Console.ReadLine()!);
+            int numero = int.Parse(Console.ReadLine()!);
 
             Console.Write("Entre o titular da conta: ");
-            conta01.NomeTitular = Console.ReadLine();
+            string titular = Console.ReadLine()!;
+            
+            Console.Write("Haverá depósito inicial (s/n)?: ");
+            char resposta = char.Parse(Console.ReadLine()!);
 
-            Console.Write("Haverá depósito inicial (s/n)? ");
-            char depositoInicial = char.Parse(Console.ReadLine()!);
-
-            if (depositoInicial.ToString() == "s")
+            if (resposta == 's' || resposta == 'S')
             {
-                Console.Write("Entre o valor de depósito inicial: ");
-                double valorDepositoInicial = double.Parse(Console.ReadLine()!, System.Globalization.CultureInfo.InvariantCulture);
-                conta01.Depositar(valorDepositoInicial);
+                Console.Write("Digite o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine()!, System.Globalization.CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(titular, numero, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(titular, numero);
             }
 
+
             Console.WriteLine("Dados da conta: ");
-            Console.WriteLine(conta01);
+            Console.WriteLine(conta);
 
             Console.Write("Entre um valor para depósito: ");
-            double valorDeposito = double.Parse(Console.ReadLine()!, System.Globalization.CultureInfo.InvariantCulture);
-            conta01.Depositar(valorDeposito);
+            double valor = double.Parse(Console.ReadLine()!, System.Globalization.CultureInfo.InvariantCulture);
+            conta.Depositar(valor);
 
             Console.WriteLine("Dados da conta atualizados: ");
-            Console.WriteLine(conta01);
-            
+            Console.WriteLine(conta);
+
             Console.Write("Entre um valor para saque: ");
-            double valorSaque = double.Parse(Console.ReadLine()!, System.Globalization.CultureInfo.InvariantCulture);
-            conta01.Sacar(valorSaque);
+            valor = double.Parse(Console.ReadLine()!, System.Globalization.CultureInfo.InvariantCulture);
+            conta.Sacar(valor);
 
-            Console.WriteLine("Dados da conta atualizados: ");
-            Console.WriteLine(conta01);
-
+            Console.WriteLine("Dados da conta atualizados: ", System.Globalization.CultureInfo.InvariantCulture);
+            Console.WriteLine(conta);
         }
     }
 
-    class Conta
+    class ContaBancaria
     {
-        private double _saldo;
         private double _taxaSaque = -5.00;
-        public int ContaId { get; set; }
-        public string? NomeTitular { get; set;}
+        public double Saldo { get; private set; }
+        public int Numero { get; private set; }
+        public string? Titular { get; set;}
 
-        public Conta()
+        // Construtor com 2 parâmetros
+        public ContaBancaria(string titular, int numero)
         {
+            Titular = titular;
+            Numero = numero;
+        }
 
+
+        // Construtor com 3 parâmetros, reaproveitando os valores do construtor acima
+        public ContaBancaria(string titular, int numero, double saldo) : this(titular, numero)
+        {
+            Saldo = saldo;
         }
 
 
@@ -59,28 +74,26 @@ namespace OrientacaoObjetos
         {
             if (valor > 0)
             {
-                _saldo += valor;
+                Saldo += valor;
             }
         }
 
         public void Sacar(double valor)
         {
-            if (valor > 0 && _saldo > valor)
+            if (valor > 0 && Saldo > valor)
             {
-                _saldo -= valor - _taxaSaque;
+                Saldo -= valor - _taxaSaque;
             }
         }
 
         public override string ToString()
         {
             return "Conta "
-            + ContaId
-            + ", "
-            + "Titular : "
-            + NomeTitular
-            + ", "
-            + "Saldo: $"
-            + _saldo.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
+            + Numero
+            + ", Titular : "
+            + Titular
+            + ", Saldo: $"
+            + Saldo.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
         }
     }
 }
