fa59685ca11323281163134cd7b8d5c37263472b
DaveAndCode <DaveAndCode@gmail.com>
2024-05-09 13:29:09 -0300

    Properties

---
+++ OrientacaoObjetos/Properties/Produto.cs
@@ -0,0 +1,46 @@
+namespace Properties
+{
+    class Produto
+    {
+        private string? _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+
+        // Properties
+        public string Nome
+        {
+            get { return _nome!;}
+            
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get { return _preco; }
+        }
+
+        public int Quantidade
+        {
+            get { return _quantidade; }
+        }
+    }
+}
---
+++ OrientacaoObjetos/Properties/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+            // Properties
+            /*
+            - São definições de métodos encapsulados, porém expondo uma sintaxe similar à de atributos e não de métodos.
+
+            - Uma propriedade é um membro que oferece uma mecanismo flexível para ler, gravar ou calcular o valor de um campo particular, As propriedades podem ser usadas como se fossem atributos públicos, mas na verdade elas são métodos especiais chamados "acessadores". Isso permite que os dados sejam acessados facilmente e ainda ajudam a promover a segurança e a flexibilidade dos métodos.
+            */
+
+            Produto produto = new Produto("TV", 900.0, 10);
+
+            produto.Nome = "TV 4K";  // Aqui usa-se a sintaxe de atributo, porém está sendo acesso com properties da classe.
+
+            Console.WriteLine(produto.Preco.ToString("F2"));
+       }
+    }
+}
