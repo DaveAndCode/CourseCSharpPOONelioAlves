589fb702efbecbf4019660e515afb606a30380e0
DaveAndCode <DaveAndCode@gmail.com>
2024-05-13 09:35:14 -0300

    Desalocação de memória - garbage collector e escopo local

---
+++ TiposDados/GarbageCollector/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Garbage collector
+            /*
+            - É um processo que automatiza o gerencimaneto de memória de um
+            programa em execução
+
+            - O garbage collector monitora os objetos alocados dinamicamente
+            pelo programa (no heap), desalocando aqueles que não estão mais
+            sendo utilizados.
+
+            - Os objetos alocados dinamicamente, quando não possuem mais
+            referência para eles, serão desalocados pelo garbage collector
+
+            - Variáveis locais são desalocadas imediatamente, assim que seu escopo
+            local sai de execução.
+            */
+        }
+    }
+}
